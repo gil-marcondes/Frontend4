d7c5358c1ffd483b46e7b32ece37cebf55469e6f
gil-marcondes <73506561+gil-marcondes@users.noreply.github.com>
2020-12-06 23:20:11 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import './App.css';
+import Lista from './Lista';
+
+class App extends Component{
+  render(){
+  return (
+  <div>
+ 
+        <Lista/>
+		
+      </div>
+	  );
+}
+}
+
+export default App;
